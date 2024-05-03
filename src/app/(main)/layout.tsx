ed1bdb99d82b1651673d66c659db61ed8ae414ee
Charles V. <charles.vogt@gmail.com>
2024-05-03 08:07:42 +0200

    Completed sidebar and toolbar

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,21 @@
+import InfoBar from '@/components/infobar';
+import Sidebar from '@/components/sidebar';
+import React, { ReactNode } from 'react';
+
+type Props = {
+    children: ReactNode;
+};
+
+const Layout = (props: Props) => {
+    return (
+        <div className='flex overflow-hidden h-screen'>
+            <Sidebar />
+            <div className='w-full'>
+                <InfoBar />
+                {props.children}
+            </div>
+        </div>
+    );
+};
+
+export default Layout;
